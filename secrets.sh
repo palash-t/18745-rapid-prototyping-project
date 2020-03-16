9e040365dba38c496ddfde600cfe48e89e390c0f
Sam Nelson <sam.nelson44@gmail.com>
2020-03-16 14:44:28 -0400

    add test data task command and secrets

---
+++ secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# AWS and DB connection secrets
+
+# Remote connection
+export USERNAME='postgres'
+export PASSWORD='admin'
+export DB_NAME='engine_db'
+export DB_HOST='localhost'
+export DB_PORT='5432'
+
+# Local connection
+export LOCAL_USERNAME='postgres'
+export LOCAL_PASSWORD='admin'
+export LOCAL_DB_NAME='engine_db'
+export LOCAL_DB_HOST='localhost'
+export LOCAL_DB_PORT='5432'
